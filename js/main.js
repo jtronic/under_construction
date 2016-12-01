398b0d3efffac7ce9958316544a9bb4b0b0fef47
jtronic <jtronic@mail.ru>
2016-12-01 19:13:16 +0300

    release

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 ﻿$(document).ready(function() {
 
-    var timer = setInterval(function() {
+    setInterval(function() {
 
         var date = new Date();
 
@@ -9,9 +9,40 @@
 
     }, 500);
 
-    /*  $(window).resize(function() {
 
-          $('.item').css("height", Number(window.innerHeight - 1).toString() + "px !important");
-      });*/
+    var timer = undefined;
+
+    $('#date').hover(function() {
+
+            _$ = $('#header');
+
+            _$.tooltip({
+                title: '<h1><span class="glyphicon  glyphicon-calendar"></span> ' + this.outerHTML + '</h1>',
+                html: true,
+                placement: 'top',
+            });
+            _$.tooltip('show');
+
+        },
+        function() {
+            $('#header').tooltip('destroy');
+
+        });
+
+    $('#time').hover(function() {
+
+            _$ = $('#header');
+
+            _$.tooltip({
+                title: '<h1><span class="glyphicon  glyphicon-time"><br/></span> ' + this.outerHTML + '</h1>',
+                html: true,
+                placement: 'top',
+            });
+            _$.tooltip('show');
+
+        },
+        function() {
+            $('#header').tooltip('destroy');
+        });
 
 });
